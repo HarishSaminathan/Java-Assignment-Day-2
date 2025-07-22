4d09c803c4dcf961938fdd0900cfb79f44612368
Harish  A S <109975544+HarishSaminathan@users.noreply.github.com>
2025-07-22 11:33:41 +0530

    OneRPattern.java

--- OneRPattern.java
+++ OneRPattern.java
@@ -1,11 +1,24 @@
 public class OneRPattern {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
-            int ones = (i == 3) ? 1 : 2;
-            int rs = (i == 1 || i == 3) ? 4 : 2;
+        printXPattern();
+        System.out.println();
+        printRPattern();
+    }
 
-            for (int j = 0; j < ones; j++) System.out.print("1 ");
-            for (int j = 0; j < rs; j++) System.out.print("R ");
+    static void printXPattern() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print((i == j || i + j == 4) ? "1" : " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printRPattern() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print((i == 1 || i == 3 || (i == 4 && (j == 1 || j == 3)) || ((i == 2 || i == 5) && (j == 1 || j == 4))) ? "R" : " ");
+            }
             System.out.println();
         }
     }
