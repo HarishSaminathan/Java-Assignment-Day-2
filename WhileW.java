203692df195715dd569e56e24e39b26d508ecf68
Harish  A S <109975544+HarishSaminathan@users.noreply.github.com>
2025-07-22 13:01:08 +0530

    WhileW.java

--- WhileW.java
+++ WhileW.java
@@ -1,1 +1,22 @@
-
+public class WhileW {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<5)
+        {
+            if(i==1 || i==3)
+            {
+                System.out.print("w" + "w");
+            }
+            else if(i==2)
+            {
+                System.out.print("w" + "w" + "w");
+            }
+            else{
+            System.out.print("w");
+            }
+            System.out.println();
+            i++;
+        }
+        
+    }
+}
